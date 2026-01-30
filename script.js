bccf37f570c6fa39416c5abd9b84b4b2a0af4b57
soloopaentendidos-sudo <soloopaentendidos@gmail.com>
2026-01-30 12:56:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-fetch('products.json')
-.then(r=>r.json())
-.then(data=>{
- const c=document.getElementById('products')
- data.forEach(p=>{
-  const d=document.createElement('div')
-  d.className='card'
-  d.innerHTML=`<h3>${p.name}</h3><p>$${p.price}</p>`
-  c.appendChild(d)
- })
+fetch('products.json').then(r=>r.json()).then(data=>{
+const c=document.getElementById('products')
+data.forEach(p=>{
+const d=document.createElement('div')
+d.className='card'
+d.innerHTML=`<h3>${p.name}</h3><p>$${p.price}</p><button>Agregar</button>`
+c.appendChild(d)
+})
 })
