6fa8a61798a61dde7b782e9aa75577a4eef8b932
soloopaentendidos-sudo <soloopaentendidos@gmail.com>
2026-01-29 21:22:31 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlc7JPTeWVKSATmRk3ZAgi42lR9Z5TJAg",
+  authDomain: "md-store-6e179.firebaseapp.com",
+  projectId: "md-store-6e179",
+  storageBucket: "md-store-6e179.firebasestorage.app",
+  messagingSenderId: "1049220648351",
+  appId: "1:1049220648351:web:b9ef4d4e73934bf59d01aa"
+};
+
+initializeApp(firebaseConfig);
---
+++ script.js
@@ -0,0 +1,11 @@
+fetch('products.json')
+.then(r=>r.json())
+.then(data=>{
+ const c=document.getElementById('products')
+ data.forEach(p=>{
+  const d=document.createElement('div')
+  d.className='card'
+  d.innerHTML=`<h3>${p.name}</h3><p>$${p.price}</p>`
+  c.appendChild(d)
+ })
+})
